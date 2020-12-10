49a10870f6151b6ecb16d202adce0a44277abe18
YvonneJansen <jansen.yv@gmail.com>
2020-12-10 16:36:50 +0100

    bug fix

--- html/setup/load_js.php
+++ html/setup/load_js.php
@@ -7,18 +7,3 @@
 
  <script type="module" src="html/js/tools/helper.js" charset="utf-8"></script>
 
-<!-- You can also load additional css here if needed -->
-
-  <?php 
-  // include js according to selected condition --> adjust as needed!
-  // the following variables can be used to load files conditionally:
-  //  - $condition (int), 
-  //  - $factor1 (string of the level name) and 
-  //  - $factor2 (if defined)
-
-  // Example
-  if ($factor1 == $config["factors"][0]["levels"][0]){ 
-    echo '<script type="module" src="html/js/visualizations/condition1.js" charset="utf-8"></script>' . PHP_EOL;
-  } else {
-    echo '<script type="module" src="html/js/visualizations/condition2.js" charset="utf-8"></script>' . PHP_EOL;
-  }?>
